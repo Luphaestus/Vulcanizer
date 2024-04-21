758257055bade777988e641ce57cd0628799cf58
Luphaestus <zak.rosin@gmail.com>
2024-04-21 12:09:14 +0100

    Project structure

---
+++ Assets/Scripts/Helper/Image/Unmount.sh
@@ -0,0 +1,17 @@
+UnmountAll() {
+  local target_dir="$1"
+
+  if [ ! -d "$target_dir" ]; then
+    echo "Error: Target directory '$target_dir' does not exist."
+    return 1
+  fi
+
+  while IFS= read -r -d '' dir; do
+    sudo umount "$dir"
+    if [ $? -eq 0 ]; then
+      echo "Successfully unmounted: $dir"
+    else
+      echo "Failed to unmount: $dir"
+    fi
+  done < <(find "$target_dir" -type d -print0 | sort -r)
+}
--- build.sh
+++ build.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+set -e
+
+COMMON_COMMANDS=()
+
+CONFIG=./config.sh
+. $CONFIG
+UI=$HELPER_DIR/UI.sh
+. $UI
+
+if [ "$EUID" -ne 0 ]; then
+  UI "!Vulcanizer be run as root. Re-running with sudo..."
+  exec sudo "$0" "$@"
+fi
+
+UI "t|Loading Scripts"
+for script in $(find $ASSETS_DIR -type f -name "*.sh"); do
+  UI "Loading: $(basename "$script")"
+  . $script
+  UI "d"
+done
+
+for cmd in "${COMMON_COMMANDS[@]}"; do
+    UI "Running: $cmd..."
+    eval "$cmd"
+done
